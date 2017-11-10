74e1b9da4ba0a42ff12693cc13eae913c90b366d
Charlie Stanard <charlie@bokkagroup.com>
2017-11-09 17:08:21 -0700

    Updating Data class tests

--- tests/DataTest.php
+++ tests/DataTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require('./src/Data.php');
+
 use PHPUnit\Framework\TestCase;
 use Bokka\Utilities\Data as Data;
 
@@ -9,16 +11,6 @@ use Bokka\Utilities\Data as Data;
 
 final class DataTest extends TestCase
 {
-    /**
-     * private $states property shouldn't be accessed directly
-     */
-    // public function testStatesPropertyCannotBeAccessed()
-    // {}
-
-    /**
-     * getStates should return a non-empty array
-     * with the first element equal to AL
-     */
     public function testGetStatesReturnsNonEmptyArray()
     {
         $states = Data::getStates();
@@ -32,14 +24,10 @@ final class DataTest extends TestCase
         //array is not empty
         $this->assertNotEmpty($states);
 
-        //should have 50 items
-        $this->assertCount(50, $states);
-
-        //first item in array should be AL
-        $firstStateAbbr = reset($states);
-        $this->assertEquals(
-            'AL',
-            $firstStateAbbr
+        //array should have key Alabama
+        $this->assertArrayHasKey(
+            'Alabama',
+            $states
         );
     }
 }
